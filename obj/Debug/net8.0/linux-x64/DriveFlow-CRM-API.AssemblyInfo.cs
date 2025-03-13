a6f17287b8cbff6a7cff95ea5a1397ffae47fbc8
whos-gabi <56652297+whos-gabi@users.noreply.github.com>
2025-03-13 23:00:54 +0200

    Final deployment fixes

--- obj/Debug/net8.0/linux-x64/DriveFlow-CRM-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/linux-x64/DriveFlow-CRM-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveFlow-CRM-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc56d8eb0cf0935ce02d25a6f210bc8b4ec3ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d99ab9a1a769c5df792b2b3e2277308e7cf738")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveFlow-CRM-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveFlow-CRM-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
