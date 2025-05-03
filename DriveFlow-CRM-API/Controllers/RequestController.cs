2f796e646c61154f65c7cc941adb23d636a90e4d
Ionut Nica <ionutxd04@gmail.com>
2025-05-03 21:37:39 +0300

    Controller full

--- DriveFlow-CRM-API/Controllers/RequestController.cs
+++ DriveFlow-CRM-API/Controllers/RequestController.cs
@@ -35,7 +35,20 @@ public class RequestController : ControllerBase
 
     /// <summary>Create a new enrollment Request for someone wishing to start their courses (Student, SchoolAdmin, SuperAdmin only).</summary>
     /// <remarks> SchoolAdmin's SchoolId must match the AutoSchoolId given as method paramether.
-    /// <para> <strong>Sample output</strong> </para> </remarks>
+    /// <para> <strong>Sample request body</strong> </para> 
+    ///     /// 
+    /// [
+    ///  {
+    ///    "id": 23523,
+    ///    "fullName": "Maria Ionescu",
+    ///    "phoneNr": "0721234234",
+    ///    "drivingCategory": "A2",
+    ///    "requestDate": "2025-10-12",
+    ///    "status": "Approved"
+    ///  }
+    ///]
+    /// 
+    /// </remarks>
     /// <response code="200">Request sent succesffully.</response>
     /// <response code="400">Empty request</response>>
     /// <response code="401">No valid JWT supplied.</response>
@@ -48,6 +61,8 @@ public class RequestController : ControllerBase
         if (requestDto == null)
             return BadRequest("Request data is required.");
         var user = await _users.GetUserAsync(User);   
+        if(user == null)
+            return Unauthorized("User not found.");
         if (User.IsInRole("SchoolAdmin") &&  user.AutoSchoolId != requestDto.AutoSchoolId)
             return Forbid("You are not authorized to create requests for this auto school.");
 
@@ -61,13 +76,16 @@ public class RequestController : ControllerBase
             PhoneNumber = requestDto.PhoneNr,
             DrivingCategory = requestDto.DrivingCategory,
             RequestDate = DateTime.UtcNow,
-            Status = "PENDING",
+            Status = "Pending",
             AutoSchoolId = requestDto.AutoSchoolId,
         };
         await _db.Requests.AddAsync(newRequest);
         await _db.SaveChangesAsync();
         return Ok("Request was sent successfully!");
     }
+
+
+
 
 
 
@@ -126,7 +144,7 @@ public class RequestController : ControllerBase
                 FullName = r.FirstName + " " + r.LastName,
                 PhoneNr = r.PhoneNumber,
                 DrivingCategory = r.DrivingCategory,
-                RequestDate = r.RequestDate.ToString("dd-MM-yyyy"),
+                RequestDate = r.RequestDate,
                 Status = r.Status
             })
             .ToListAsync();
@@ -134,6 +152,87 @@ public class RequestController : ControllerBase
     }
 
 
+    // ────────────────────────────── UPDATE REQUEST ──────────────────────────────
+    /// <summary>Update the status of a request (SchoolAdmin, SuperAdmin only).</summary>
+    /// <remarks> SchoolAdmin's SchoolId must match the AutoSchoolId given as method paramether.
+    /// The only status values allowed are: APPROVED, REJECTED, PENDING.
+    /// That's the only thing that is going to be changed.
+    /// <para>Sample request body</para>
+    /// 
+    ///     /// 
+    /// [
+    ///  "requestId":23523,
+    ///  {
+    ///    "id": 23523,
+    ///    "fullName": "Maria Ionescu",
+    ///    "phoneNr": "0721234234",
+    ///    "drivingCategory": "A2",
+    ///    "requestDate": "2025-10-12",
+    ///    "status": "PENDING"
+    ///  }
+    ///]
+    /// </remarks>
+    /// <response code="200">Request updated successfully.</response>
+    /// <response code="400">RequestId or the new Status was not a valid value</response>>
+    /// <response code="401">No valid JWT supplied.</response>
+    /// <response code="403">User is forbidden from seeing the requests of this auto school.</response>
+
+
+    [HttpPut("update/{requestId}")]
+    public async Task<IActionResult> UpdateRequestStatus(int requestId, RequestDto requestDto)
+    {
+        if (requestId <= 0)
+            return BadRequest("Invalid request ID.");
+
+        if( requestDto== null)
+        {
+            return BadRequest("Request data is required.");
+        }
+
+        var user = await _users.GetUserAsync(User);
+        if (user == null)
+            return Unauthorized("User not found.");
+        if (!(User.IsInRole("SchoolAdmin") && user.AutoSchoolId == requestId))
+            return Forbid("You are not authorized to update this request.");
+
+        var request = await _db.Requests.FindAsync(requestId);
+        if (request == null)
+            return NotFound("Request not found.");
+
+        request.Status = requestDto.Status; // Update the status of the request, that's all we do here.
+
+        await _db.SaveChangesAsync();
+        return Ok("Request status updated successfully.");
+    }
+
+
+    // ────────────────────────────── DELETE REQUEST ──────────────────────────────
+    /// <summary>Delete a request (SchoolAdmin, SuperAdmin only).</summary>
+    /// 
+    /// <response code="200">Requests deleted successfully.</response>
+    /// <response code="400">Request does not exist</response>
+    /// <response code="401">No valid JWT supplied.</response>
+    /// <response code="403">User is forbidden from seeing the requests of this auto school.</response>
+
+    [HttpDelete("delete/{requestId}")]
+    [Authorize(Roles = "SchoolAdmin,SuperAdmin")]
+
+    public async Task<IActionResult> DeleteRequest(int requestId)
+    {
+        if (requestId <= 0)
+            return BadRequest("Invalid request ID.");
+        var user = await _users.GetUserAsync(User);
+        if (user == null)
+            return Unauthorized("User not found.");
+        if (!(User.IsInRole("SchoolAdmin") && user.AutoSchoolId == requestId))
+            return Forbid("You are not authorized to delete this request.");
+        var request = await _db.Requests.FindAsync(requestId);
+        if (request == null)
+            return NotFound("Request not found.");
+        _db.Requests.Remove(request);
+        await _db.SaveChangesAsync();
+        return Ok("Request deleted successfully.");
+    }
 }
 
 
@@ -144,7 +243,7 @@ public sealed class RequestDto
     
     public string PhoneNr { get; init; } = default!;    
     public string? DrivingCategory { get; init; } = default!;
-    public string? RequestDate { get; init; } = default;
+    public DateTime RequestDate { get; init; } = default;
     public string Status { get; init; } = default!;
 
     public int? AutoSchoolId { get; init; } = default!;  
