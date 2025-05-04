0d0744531a4119e2c92d2988ec21dd5b99dcdf16
Ionut Nica <ionutxd04@gmail.com>
2025-05-04 21:38:44 +0300

    need to talk

--- DriveFlow-CRM-API/Controllers/RequestController.cs
+++ DriveFlow-CRM-API/Controllers/RequestController.cs
@@ -167,6 +167,49 @@ public class RequestController : ControllerBase
     }
 
 
+
+    // ────────────────────────────── UPDATE REQUEST ──────────────────────────────
+    /// <summary>Update the status of a request by opening a special menu 
+    /// for that request only via its RequestId</summary>
+    /// <remarks>
+    /// If the user is a SchoolAdmin, then his SchoolId must match the parameter SchoolId.
+    /// The only status values allowed are: APPROVED, REJECTED, PENDING.
+    /// That's the only thing that is going to be changed.
+    /// </remarks>
+    /// <response code="200">Request updated successfully.</response>
+    /// <response code="401">No valid JWT supplied.</response>
+    /// <response code="403">User is forbidden from seeing the requests of this auto school.</response>
+
+
+    [HttpPut("update/{requestId}/updateRequestStatus")]
+    public async Task<IActionResult> UpdateRequestStatus(int requestId)
+    {
+        if (requestId <= 0)
+            return BadRequest("Invalid request ID.");
+
+
+        var user = await _users.GetUserAsync(User);
+        if (user == null)
+            return Unauthorized("User not found.");
+        if (!(User.IsInRole("SchoolAdmin") && user.AutoSchoolId == requestId))
+            return Forbid("You are not authorized to update this request.");
+
+        var request = await _db.Requests.FindAsync(requestId);
+        if (request == null)
+            return NotFound("Request not found.");
+
+        return Ok(new RequestDto
+        {
+            RequestId = requestId,
+            Status = request.Status,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            PhoneNr = request.PhoneNumber,
+            DrivingCategory=request.DrivingCategory,
+            RequestDate=request.RequestDate,
+        });
+    }
+
     // ────────────────────────────── UPDATE REQUEST ──────────────────────────────
     /// <summary>Update the status of a request (SchoolAdmin, SuperAdmin only).</summary>
     /// <remarks>
